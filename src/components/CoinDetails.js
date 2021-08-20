32b2bd8b2b6a147c89d154ddca00497f846f5e79
Kausik Das <kausik007007@gmail.com>
2021-08-20 22:26:46 +0530

    chart added

--- src/components/CoinDetails.js
+++ src/components/CoinDetails.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { Bar, Line } from "react-chartjs-2";
 
 import { singleCoin, historialPriceData } from "./API/API";
+
+let chartData = [];
+let chartDataLength = [];
 
 const CoinDetails = () => {
   const params = useParams();
@@ -16,6 +20,22 @@ const CoinDetails = () => {
     historialPriceData(params.id)
   );
   const allHistoricalData = historicalPrice.data;
+
+  allHistoricalData &&
+    allHistoricalData.prices.map((layer1) => {
+      return layer1.map((num) => {
+        return !Number.isInteger(num) ? chartData.push(num) : null;
+      });
+    });
+
+  const dataLength = chartData.length;
+
+  for (let i = 0; i <= dataLength; i++) {
+    chartDataLength.push(i);
+  }
+
+  console.log(chartDataLength);
+  // console.log(dataLength);
 
   //   console.log(data);
   //   console.log(allHistoricalData && allHistoricalData.prices);
@@ -31,23 +51,48 @@ const CoinDetails = () => {
         </div>
       )}
 
-      {allHistoricalData &&
-        allHistoricalData.prices.map((layer1) => {
-          return (
-            <>
-              {layer1 &&
-                layer1.map((num) => {
-                  return (
-                    <>
-                      <div>
-                        {!Number.isInteger(num) ? num.toFixed(2) : null}
-                      </div>
-                    </>
-                  );
-                })}
-            </>
-          );
-        })}
+      <div>
+        <Line
+          width={400}
+          height={400}
+          data={{
+            labels: chartDataLength,
+            datasets: [
+              {
+                label: "Price",
+                data: chartData,
+                backgroundColor: [
+                  // "rgba(75, 192, 192, 0.2)",
+                  "rgba(255, 99, 132, 0.2)",
+                  // "rgba(54, 162, 235, 0.2)",
+                  // "rgba(255, 206, 86, 0.2)",
+                  // "rgba(153, 102, 255, 0.2)",
+                  // "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                  // "rgba(75, 192, 192, 1)",
+                  "rgba(255, 206, 86, 1)",
+                  // "rgba(54, 162, 235, 1)",
+                  // "rgba(255, 99, 132, 1)",
+                  // "rgba(153, 102, 255, 1)",
+                  // "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+              },
+            ],
+          }}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true,
+                // alignToPixels: true,
+              },
+              x: {},
+            },
+          }}
+        />
+      </div>
     </>
   );
 };
